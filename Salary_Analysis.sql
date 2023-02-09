23183f7f8b512478063a8b983e3e25ff4df8a23d
martin kilombe <martinkilombe@gmail.com>
2023-02-09 11:51:21 +0300

    second commit

--- Salary_Analysis.sql
+++ Salary_Analysis.sql
@@ -1,6 +1,18 @@
 SELECT * FROM data_scientist_salary.data_scientist_salaries;
-#Droping the "my unkowncolumn" that came imported with the csv file.
+#1. Droping the "my unkowncolumn" that came imported with the csv file -- insignificant row
 ALTER TABLE data_scientist_salary.data_scientist_salaries
 DROP COLUMN MyUnknownColumn;
 
-#Adding an id column that is going to serve as the primary key for the table .ID column will be generated from the job title column
+#2. Adding an ID column that is going to serve as the primary key for the table .ID column will be generated from the job title column
+ALTER TABLE data_scientist_salary.data_scientist_salaries
+ADD COLUMN ID VARCHAR(32) ;
+#Creating a hash(md5) that is goint to be used as the primary key
+UPDATE data_scientist_salary.data_scientist_salaries
+SET ID = MD5(job_title)
+WHERE ID IS NULL;
+#Setting the ID as the primary key
+ALTER TABLE data_scientist_salary.data_scientist_salaries
+ADD PRIMARY KEY (ID);
+
+
+
