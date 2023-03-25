bd9428f5b4a795e24c846af5bd3f855bf1d1be30
martin kilombe <martinkilombe@gmail.com>
2023-03-25 13:43:14 +0300

    Cleaned code

--- Salary_Analysis.sql
+++ Salary_Analysis.sql
@@ -6,15 +6,17 @@ DROP COLUMN MyUnknownColumn;
 #2. Adding an ID column that is going to serve as the primary key for the table .ID column will be generated from the job title column
 ALTER TABLE data_scientist_salary.data_scientist_salaries
 ADD COLUMN ID VARCHAR(32) ;
-#Creating a hash(md5) that is goint to be used as the primary key
+
+#3.Creating a hash(md5) that is goint to be used as the primary key
 UPDATE data_scientist_salary.data_scientist_salaries
 SET ID = MD5(job_title)
 WHERE ID IS NULL;
-#Setting the ID as the primary key
+
+#4.Setting the ID as the primary key
 ALTER TABLE data_scientist_salary.data_scientist_salaries
 ADD PRIMARY KEY (ID);
 
-#3. Basic Analysis
+#B. Basic Analysis
 #-Arranging the salaries based on job_titles
 SELECT ID,job_title, salary_in_usd FROM data_scientist_salary.data_scientist_salaries
 ORDER BY job_title;
